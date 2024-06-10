accab6e323bb1697fdffd35f62b71d464ae3327f
Fady <Fady@Fadyio.com>
2024-06-10 17:38:29 +0300

    git init

---
+++ locals.tf
@@ -0,0 +1,16 @@
+locals {
+  project = {
+    "fem-eci-project" = {
+      description = "Example description of project"
+    }
+  }
+
+  workspace = {
+    "fem-eci-tfe" = {
+      description    = "Example automation workspace for Terraform Cloud."
+      execution_mode = "local"
+      project_id     = module.project["fem-eci-project"].id
+    }
+  }
+}
+
