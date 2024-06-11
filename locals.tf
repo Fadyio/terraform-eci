98d6c11aed3d574594f117ba5727d32e069e241d
Fady <Fady@Fadyio.com>
2024-06-11 16:07:44 +0300

    Add the new worksapce for github

--- locals.tf
+++ locals.tf
@@ -7,10 +7,16 @@ locals {
 
   workspace = {
     "fem-eci-tfe" = {
-      description    = "Example automation workspace for Terraform Cloud."
-      execution_mode = "remote"
-      project_id     = module.project["fem-eci-project"].id
-      vcs_repo_identifier = "Fadyio/terraform-eci"
+      description         = "Example automation workspace for Terraform Cloud."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/terraform-eci"
+    }
+    "fem-eci-github" = {
+      description         = "Example automation workspace for Terraform Cloud and github."
+      execution_mode      = "local"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/terraform-github"
     }
   }
 }
